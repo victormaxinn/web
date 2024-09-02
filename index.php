8b47f1414123289baa6665bfcd9a4138abd8810a
Victorvhsj <66342152+victormaxinn@users.noreply.github.com>
2024-09-02 14:18:57 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("templates/header.php"); ?>
+  <br/>
+   <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">"Bienvenid@
+        A la Direccion General Del Registro Civil Del Estado de Jalisco"<br/>
+        
+        Por favor Ingrese Los Datos de la Oficilia</h1>
+      <p class="col-md-8 fs-4">Usuario : <?php echo $_SESSION['usuario'];?>.</p>
+      <button class="btn btn-primary btn-lg" type="button">!Hola Amig@!</button>
+    </div>
+    </div>
+
+    <?php include("templates/footer.php"); ?>
